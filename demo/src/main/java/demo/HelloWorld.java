d5459b3291f7742e1f3ab1a988b950010c41e883
John Thompson <fly.yamaha@gmail.com>
2015-03-11 12:26:17 -0400

    Initial add

---
+++ demo/src/main/java/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 3/11/15.
+ */
+@Component
+public class HelloWorld {
+    public void sayHello(){
+        System.out.println("Hello World!!!!");
+    }
+}
